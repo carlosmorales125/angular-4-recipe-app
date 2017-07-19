7effb8ca14ac0a8adf993f18a21f051c55a12764
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2017-07-18 21:22:43 -0400

    added jquery to project and created list of recipes in view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('Apple Pie', 'Apples baked in pie crust', 'https://images-gmi-pmc.edge-generalmills.com/597526e8-69bf-45a2-9b3c-772507d77087.jpg')
+  ];
 
   constructor() { }
 

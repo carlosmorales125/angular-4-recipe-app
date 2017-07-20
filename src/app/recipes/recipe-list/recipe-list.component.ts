a6fced8d6d96f3250a11b01c072ae834d10fc8dd
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2017-07-19 21:13:42 -0400

    now passing data between the recipe-item and recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -23,9 +23,15 @@ export class RecipeListComponent implements OnInit {
       )
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+    onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
+    }
 
 }

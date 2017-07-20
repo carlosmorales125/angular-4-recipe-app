06fb6ab8a425a31d1d4e66d23a9f4217a828b32c
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2017-07-19 20:36:44 -0400

    moved recipe items to the recipe items component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-      new Recipe('Apple Pie', 'Apples baked in pie crust', 'https://images-gmi-pmc.edge-generalmills.com/597526e8-69bf-45a2-9b3c-772507d77087.jpg')
+      new Recipe('Apple Pie',
+          'Apples baked in pie crust',
+          'https://images-gmi-pmc.edge-generalmills.com/597526e8-69bf-45a2-9b3c-772507d77087.jpg'
+      ),
+      new Recipe('Pumkin Pie',
+          'Processed pumkin baked in pie crust',
+          'https://images-gmi-pmc.edge-generalmills.com/db388a54-759d-4c6b-a787-44767d29273e.jpg'
+
+      ),
+      new Recipe('Pecan Pie',
+          'Sweet pie filling with whole pecans',
+          'https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/d/2000/decadent-chocolate-pecan-pie.ashx?vd=20170302T170601Z&ir=1&width=2000&height=1125&crop=auto&quality=75&hash=66C52C9733C33A2DA3C828138F472BDC8C715499'
+      )
   ];
 
   constructor() { }
